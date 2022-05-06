7066f12571781386e9979116ce11dcfd4dc7c145
omerfe <omerfe.eker@gmail.com>
2022-05-06 21:32:32 +0300

    FeedFromBackend

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,8 +1,14 @@
 import { RefreshIcon } from '@heroicons/react/outline'
 import React from 'react'
+import { Tweet } from '../typings'
 import TweetBox from './TweetBox'
+import TweetComponent from '../components/Tweet'
 
-function Feed() {
+interface Props {
+  tweets: Tweet[]
+}
+
+function Feed({tweets}: Props) {
   return (
     <div className='col-span-7 lg:col-span-5 border-x'>
         <div className='flex items-center justify-between'>
@@ -10,9 +16,14 @@ function Feed() {
             <RefreshIcon className='h-8 w-8 cursor-pointer text-twitter mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125'/>
         </div>
 
-        {/* Tweetbox */}
         <div>
             <TweetBox />
+        </div>
+
+        <div>
+            {tweets.map(tweet => (
+              <TweetComponent key={tweet._id} tweet={tweet}/>
+            ))}
         </div>
     </div>
   )
