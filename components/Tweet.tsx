97b596d008c47a620310e9da9f4e441d2f4f1d4f
omerfe <omerfe.eker@gmail.com>
2022-05-07 12:27:37 +0300

    Comments,react-hot-toast Integrated

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import { Tweet } from '../typings'
+import React, { useEffect, useState } from 'react'
+import { Tweet, Comment } from '../typings'
 import TimeAgo from 'react-timeago'
 import { ChatAlt2Icon, HeartIcon, SwitchHorizontalIcon, UploadIcon } from '@heroicons/react/outline'
+import { fetchComments } from '../utils/fetchComments'
 
 interface Props {
   tweet: Tweet
 }
 
 function Tweet({ tweet }: Props) {
+  const [comments, setComments] = useState<Comment[]>([])
+
+  const refreshComments = async () => {
+    const comments: Comment[] = await fetchComments(tweet._id)
+    setComments(comments);
+  }
+
+  useEffect(() => {
+    refreshComments();
+  }, [])
+
   return (
     <div className='flex flex-col space-x-3 border-y p-5 border-gray-100'>
       <div className="flex space-x-3">
@@ -42,7 +54,7 @@ function Tweet({ tweet }: Props) {
       <div className='flex mt-5 justify-between'>
           <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
               <ChatAlt2Icon className='h-5 w-5'/>
-              <p>5</p>
+              <p>{comments.length}</p>
           </div>
           <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
                 <SwitchHorizontalIcon className='h-5 w-5'/>
@@ -54,6 +66,30 @@ function Tweet({ tweet }: Props) {
                 <UploadIcon className='h-5 w-5' />
           </div>
       </div>
+
+      {/* Comment Box Logic */}
+
+      {comments?.length > 0 && (
+        <div className='my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5'>
+          {comments.map((comment) => (
+            <div key={comment._id} className='relative flex space-x-2'>
+              <hr className='absolute left-5 top-10 h-8 border-x border-twitter/30'/>
+              <img className='mt-2 h-7 w-7 object-cover rounded-full' src={comment.profileImg} alt="pp" />
+              <div>
+                <div className='flex items-center space-x-1'>
+                  <p className='mr-1 font-bold'>{comment.username}</p>
+                  <p className='hidden text-sm text-gray-500 lg:inline'>@{comment.username.replace(/\s+/g, '').toLowerCase()}</p>
+                  <TimeAgo 
+                    className='text-sm text-gray-500'
+                    date={comment._createdAt}
+                  />
+                </div>
+                <p>{comment.comment}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
